4329bb61d00ba0c3a26e515a27f78b410e630570
Chris Drackett <chris@drackett.com>
2018-04-15 15:14:39 -0700

    starting on new demo site

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,35 +1,63 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 
+import { css } from 'glamor'
+
 import Box from './Box'
 
 import Tailor from '../../src'
 
 class Demo extends React.Component {
   render() {
+    const styles = {
+      grid: {
+        maxWidth: 1200,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        display: 'grid',
+        gridTemplateColumns: '1fr 3fr',
+        gridTemplateRows: 'auto',
+        gridTemplateAreas: '"header header" "sidebar content" "footer footer"',
+      },
+      header: {
+        gridArea: 'header',
+      },
+      sidebar: {
+        gridArea: 'sidebar',
+      },
+      content: {
+        gridArea: 'content',
+      },
+      footer: {
+        gridArea: 'footer',
+      },
+    }
+
     return (
-      <React.Fragment>
-        <Tailor isSingleLine>
-          <h1>Large Headline</h1>
-        </Tailor>
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          <Box width={800} height={200}>
-            <Tailor isSingleLine>Short Text</Tailor>
-          </Box>
-          <Box width={800} height={200}>
-            <Tailor isSingleLine>Shorty</Tailor>
-          </Box>
-          <Box width={800} height={200}>
-            <Tailor isSingleLine>Short Texty</Tailor>
-          </Box>
-          <Box width={160} height={200}>
-            <Tailor isSingleLine>Much Longer Text</Tailor>
-          </Box>
-          <Box width={160} height={200}>
-            <Tailor isSingleLine>Much Much Much Longer Text</Tailor>
-          </Box>
-        </div>
-      </React.Fragment>
+      <div {...css(styles.grid)}>
+        <section {...css(styles.header)}>
+          <h1>
+            <Tailor>React Tailor</Tailor>
+          </h1>
+        </section>
+        <section {...css(styles.sidebar)}>
+          <ul>
+            <li>
+              <Tailor>Short</Tailor>
+            </li>
+            <li>
+              <Tailor>Longer</Tailor>
+            </li>
+            <li>
+              <Tailor>Even Longer</Tailor>
+            </li>
+            <li>
+              <Tailor>Short</Tailor>
+            </li>
+          </ul>
+        </section>
+        <section {...css(styles.content)}>Content</section>
+      </div>
     )
   }
 }
